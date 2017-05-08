8772cf5e559794eab047518036d8608a50300daf
Glenn <zaku0225@gmail.com>
2017-05-08 23:58:40 +0800

    理解 remove 的概念

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,6 @@
 function TodoList() {
   this.$elem = this.buildElem()
+  this.todos = []
   this.bindEvents() 
 }
 TodoList.prototype.buildElem = function() {
@@ -17,41 +18,67 @@ TodoList.prototype.buildElem = function() {
 }
 TodoList.prototype.bindEvents = function() {
   var button = this.$elem.querySelector('button')
-  button.addEventListener('click', this.onClick.bind(this))
+  button.addEventListener('click', this.onCreateTodo.bind(this))
 }
-TodoList.prototype.onClick = function() {
-  var input = this.$elem.querySelector('input')
-  var value = input.value
-  var todo = new TodoItem(value)
+TodoList.prototype.onCreateTodo = function() {
+
+  // 建立 newTodo
+  var input     = this.$elem.querySelector('input')
+  var title     = input.value
+  var newTodo = new TodoItem(title)
 
   var ul = this.$elem.querySelector('ul')
-  ul.appendChild(todo)
+  ul.appendChild(newTodo.$elem)
+
+  // 將新 Todo 推到 todos 陣列 
+  this.todos.push(newTodo)
+
+  // 增加新屬性 form，所屬的 TodoList
+  newTodo.father = this
+  // console.log(todoChild)
+
   // reset input value
   input.value = ""
+}
 
+// General
+general = {
+  globalID: 0
 }
-function TodoItem(value) {
-  this.$elem = this.buildElem(value)
+function TodoItem(title) {
+  var id = general.globalID ++
+
+  this.id    = id
+  this.$elem = this.buildElem(id, title)
   this.bindEvents()
-  return this.$elem
+  return this
 }
-TodoItem.prototype.buildElem = function(value) {
-  var $li   = document.createElement('li'),
-      $span = document.createElement('span'),
-      $btn  = document.createElement('button')
-  $span.textContent = value
+TodoItem.prototype.buildElem = function(id, title) {
+  var $li       = document.createElement('li'),
+      $label    = document.createElement('label'),
+      $checkbox = document.createElement('input'),
+      $btn      = document.createElement('button')
+  $label.textContent = title
+  $label.setAttribute('for', id)
+  $checkbox.setAttribute('type', 'checkbox')
+  $checkbox.setAttribute('id', id)
   $btn.textContent = '已完成'
   
-  $li.append($span)
+  $li.append($checkbox)
+  $li.append($label)
   $li.append($btn)
+
   return $li
 }
 TodoItem.prototype.bindEvents = function() {
   var button = this.$elem.querySelector('button')
-  button.addEventListener('click', this.removeItem.bind(this))
+  button.addEventListener('click', this.onRemoveTodo.bind(this))
 }
-TodoItem.prototype.removeItem = function() {
-  console.log('remove')
+TodoItem.prototype.onRemoveTodo = function() {
+  var todoArray = this.father.todos,
+      todoId    = this.id
+
+  todoArray[todoId].$elem.remove()
 }
 
 function App() {
